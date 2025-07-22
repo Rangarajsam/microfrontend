494937a030e2a15f29f2efbbaf6ea4f20233c880
rangaraj.s <rangaraj.sam@gmail.com>
2025-07-22 08:06:51 +0530

    initial setup

---
+++ packages/container/src/App.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { mount } from 'marketing/MarketingApp';
+
+// console.log(mount);
+
+// export default () => {
+//   return <h1>Hi there!</h1>;
+// };
+
+
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+export default () => {
+    return (
+      <div>
+        <h1>Hi, I am the container App</h1>
+        <hr />
+        <MarketingApp />
+      </div>
+    )
+}
